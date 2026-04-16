8be3108026c59b139441a0bb20e7fed4b5978c51
Nikto-code <i.bumaga@bk.ru>
2026-04-16 13:13:45 +0300

    DAY A

---
+++ task5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        Write("Введите трехзначное число: ");
+        int number = int.Parse(ReadLine());
+
+        int first = number / 100;
+        int second = (number / 10) % 10;
+        int third = number % 10;
+
+        int result = first * 100 + third * 10 + second;
+
+        WriteLine($"Результат: {result}");
+    }
+}
